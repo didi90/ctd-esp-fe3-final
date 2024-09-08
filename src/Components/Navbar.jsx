75b0008cee2c3abcf61441276490b489f02fb5fb
diamar905 <diamar.905@gmail.com>
2024-09-08 17:59:31 -0500

    Primer avance

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { routes } from "../Components/utils/routes";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
-
+  const navigate = useNavigate();
   return (
-    <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+<nav className="navbar">
+      <button onClick={() => navigate(-1)}>🔙</button>
+      <Link to={routes.home}>
+        <h4>Home</h4>
+      </Link>
+      <Link to={routes.favs}>
+        <h4>Fav</h4>
+      </Link>
+      <Link to={routes.detail}>
+        <h4>Detail</h4>
+      </Link>
+      <Link to={routes.contact}>
+        <h4>Contact</h4>
+      </Link>
     </nav>
-  )
-}
-
-export default Navbar
+  );
+};
+export default Navbar;
